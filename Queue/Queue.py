62162e36cb3a83e926fab92c8b66dd38bf97ee90
PengInGitHub <felixdeutschland@hotmail.com>
2018-08-11 15:22:15 +0200

    refactor Queue

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -7,13 +7,10 @@ Make sure you pass the test cases too!"""
 class Queue:
     def __init__(self, head=None):
         self.storage = [head]
-
     def enqueue(self, new_element):
         self.storage.append(new_element)
-
     def peek(self):
-        return self.storage[0] 
-
+        return self.storage[0]
     def dequeue(self):
         return self.storage.pop(0)
     
